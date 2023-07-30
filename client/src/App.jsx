a6da81650af571386faf59ca76f09efda8798c84
Abhiman Ranaweera <abhiman.max@gmail.com>
2023-07-30 18:21:22 +0530

    feat: ui improvements

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import graphQLLogo from "./assets/graphQL.png";
+import apolloLogo from "./assets/apollo.png";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -15,21 +17,26 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <a href="https://graphql.org/" target="_blank">
+          <img src={graphQLLogo} className="logo react" alt="React logo" />
+        </a>
+        <a href="https://www.apollographql.com/" target="_blank">
+          <img src={apolloLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Apollo GraphGL Server - Client Demo</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        The Apollo GraphQL Server-Client Demo is an application that showcases
+        the integration of both a GraphQL server and a GraphQL client using the
+        Apollo ecosystem.
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
