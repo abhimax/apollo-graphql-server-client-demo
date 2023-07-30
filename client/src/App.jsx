f0d73c32ef1211df5f752b49437f4331c48c3aac
Abhiman Ranaweera <abhiman.max@gmail.com>
2023-07-30 18:38:05 +0530

    feat: implement apollo client, connect with server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,9 +4,24 @@ import viteLogo from "/vite.svg";
 import graphQLLogo from "./assets/graphQL.png";
 import apolloLogo from "./assets/apollo.png";
 import "./App.css";
+import { gql, useQuery } from "@apollo/client";
+
+// Define a GraphQL query
+const GET_DATA = gql`
+  query GetData {
+    data {
+      id
+      name
+      description
+    }
+  }
+`;
 
 function App() {
-  const [count, setCount] = useState(0);
+  // Use the Apollo useQuery hook to fetch data
+  const { loading, error, data } = useQuery(GET_DATA);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
 
   return (
     <>
@@ -26,9 +41,17 @@ function App() {
       </div>
       <h1>Apollo GraphGL Server - Client Demo</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        {/* <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
+        </button> */}
+        <ul>
+          {data.data.map((item) => (
+            <li key={item.id}>
+              <h3>{item.name}</h3>
+              <p>{item.description}</p>
+            </li>
+          ))}
+        </ul>
       </div>
       <p className="read-the-docs">
         The Apollo GraphQL Server-Client Demo is an application that showcases
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// Create an Apollo Client instance
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql", // Replace with your GraphQL server URL
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>
+);
